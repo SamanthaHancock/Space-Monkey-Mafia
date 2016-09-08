77202bafb8968d079dcb1c1319885d7ae6c90f75
Howard, Samuel (Apprentice Software Developer) <samuel.howard@sky.uk>
2016-09-08 14:19:44 +0100

    Login page
    Login page very simply done ready for connection to DB

---
+++ public/webjs.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#hello').click(function(){
+    var inputUsername = $('#username').val();
+    var inputpass = $('#password').val();
+    window.location.href = "http://www.google.com";
+  });
+});
